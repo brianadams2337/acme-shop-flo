42b5f88ed2f8b71b4936b97f20aa18a9c70a741f
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-08-28 12:29:36 +0200

    Display shop switcher

---
+++ components/layout/header/HeaderMetaBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-primary h-[30px] w-full">
+    <div class="flex h-full justify-end">
+      <ShopSwitcher class="bg-primary mr-2 text-white" />
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="flex min-h-screen flex-col text-primary antialiased anchor-scrolling-none">
+    <HeaderMetaBar />
     <AppHeader
       :fetching-categories="fetchingCategories"
       :root-categories="

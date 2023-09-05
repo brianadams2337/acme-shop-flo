5ba210d9d0c2acdf47708e0115abb7d997353794
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-05 12:56:59 +0200

    Move FilterSlideInLogic to composable

--- components/filter/FilterSlideIn.vue
+++ components/filter/FilterSlideIn.vue
@@ -21,105 +21,8 @@
       </div>
     </template>
     <template #slideInBody>
-      <div class="my-8 divide-y">
-        <FilterGroup
-          :badge="state.size.length"
-          :label="$t('filter.size')"
-          @click:reset="resetFilter('size')">
-          <MultipleSelectionList
-            v-model="state.size"
-            :items="availableFilterValues.size"
-            class="flex flex-wrap"
-            name="FilterSelectSize">
-            <template #item="{ item, toggleItem, isActive }">
-              <AppButton
-                :class="
-                  isActive
-                    ? 'border-[#2d2c2f] !bg-white text-primary'
-                    : 'border-gray-350 text-[#5c5b5f]'
-                "
-                class="mb-2 mr-2 border-2 px-3 py-2 text-xs font-medium"
-                type="secondary"
-                no-padding
-                @click="toggleItem(item)"
-                >{{ item.displayName }}
-              </AppButton>
-            </template>
-          </MultipleSelectionList>
-        </FilterGroup>
-
-        <FilterGroup
-          :badge="state.brand.length"
-          :label="$t('filter.brands')"
-          @click:reset="resetFilter('brand')">
-          <MultipleSelectionList
-            v-model="state.brand"
-            :items="availableFilterValues.brand"
-            :limit="6"
-            selected-first
-            class="grid grid-cols-2 gap-3"
-            name="FilterSelectBrand">
-            <template #item="{ item }">
-              <li class="mb-2 list-none">
-                <CheckBox
-                  :id="item.displayName"
-                  v-model="state.brand"
-                  :item="item"
-                  :label="item.displayName" />
-              </li>
-            </template>
-          </MultipleSelectionList>
-        </FilterGroup>
-
-        <FilterGroup
-          :badge="state.color.length"
-          :label="$t('filter.colors')"
-          @click:reset="resetFilter('color')">
-          <MultipleSelectionList
-            v-model="state.color"
-            :items="availableFilterValues.color"
-            class="flex flex-wrap gap-3"
-            name="FilterSelectColors">
-            <template #item="{ item, toggleItem, isActive }">
-              <button
-                :aria-label="`select color ${item.displayName}`"
-                class="appearance-none focus:outline-none"
-                @click="toggleItem(item)">
-                <ColorChip
-                  data-test-id="filter-color-circle"
-                  :color="{
-                    id: +item.value,
-                    value: `${item.value}`,
-                    label: item.displayName,
-                  }"
-                  class="h-5 w-5"
-                  :is-active="isActive" />
-              </button>
-            </template>
-          </MultipleSelectionList>
-        </FilterGroup>
-
-        <FilterGroup
-          v-if="hasPriceRange"
-          :label="$t('filter.price')"
-          :show-action="
-            (activeFilters.maxPrice || activeFilters.minPrice) && priceChanged
-          "
-          @click:reset="resetFilter('prices')">
-          <RangeSlider
-            v-model="state.prices"
-            :max="maxPrice"
-            :min="minPrice"
-            :currency-code="currencyCode"
-            :locale="locale" />
-        </FilterGroup>
-
-        <FilterGroup v-if="isSaleActive" :label="$t('filter.only_sale')">
-          <template #action>
-            <Toggle v-model="state.sale" />
-          </template>
-        </FilterGroup>
-      </div>
+      <FilterSlideInBody :active-filters="activeFilters" :filters="filters"/>
+      <div>Hallo</div>
     </template>
 
     <template #slideInActions>
@@ -149,17 +52,7 @@
 </template>
 
 <script setup lang="ts">
-import {
-  CentAmount,
-  getActiveFilters,
-  getFilterablePriceValue,
-  getGroupedFilterableValues,
-  groupFiltersByKey,
-  ProductFilter,
-  TransformedFilter,
-  transformMinAndMaxPriceToFilter,
-  transformStateToFilters,
-} from '@scayle/storefront-nuxt'
+import { TransformedFilter, ProductFilter} from '@scayle/storefront-nuxt';
 
 const props = defineProps({
   filters: {
@@ -167,7 +60,7 @@ const props = defineProps({
     default: () => [],
   },
   activeFilters: {
-    type: Object as PropType<Record<string, any>>,
+    type: Object as PropType<Record<string, TransformedFilter[]>>,
     default: () => {},
   },
   filteredCount: {
@@ -184,144 +77,26 @@ const props = defineProps({
   },
 })
 
-const emit = defineEmits([
-  'filter:apply',
-  'filter:state-changed',
-  'filter:reset',
-])
-
 const { filters: filterableValues, activeFilters } = toRefs(props)
-const { toggle } = useSlideIn('FilterSlideIn')
-const supportedFilters = ['brand', 'size', 'color', 'prices', 'sale']
-// const { trackFilterFlyout } = useTrackingEvents()
-const trackFilterFlyout = (..._args: any) => {}
 
-interface FilterState {
-  brand: []
-  size: []
-  color: []
-  prices: [CentAmount, CentAmount]
-  sale: boolean
-}
+const emit = defineEmits<{
 
-const availableFilterValues = computed(() =>
-// TODO fix in core
-  getGroupedFilterableValues(supportedFilters, filterableValues.value) as Record<string, TransformedFilter[]>,
-)
+  ( e: 'filter:apply', filter: Record<string, any>): void,
+  ( e: 'filter:state-changed', filter: Record<string, any>): void,
+  ( e: 'filter:reset'): void,
+}>()
 
-const minPrice = computed(() =>
-  getFilterablePriceValue(availableFilterValues.value, 'min'),
-)
-
-const maxPrice = computed(() =>
-  getFilterablePriceValue(availableFilterValues.value, 'max'),
-)
-
-const hasPriceRange = computed(() => minPrice.value !== maxPrice.value)
-
-// initial result subset
-const initialState = computed<FilterState>(() => ({
-  size: [],
-  brand: [],
-  color: [],
-  prices: [minPrice.value, maxPrice.value],
-  sale: false,
-}))
-
-// user's selected conditions
-const state = reactive(useClone(initialState.value)) as FilterState
-
-const setActiveFiltersInState = (filters: TransformedFilter[]) => {
-  /**
-   * https://aboutyou.atlassian.net/browse/SCPF-4018
-   * Reset to initial state values (except price), then apply only the changes
-   */
-  state.size = []
-  state.brand = []
-  state.color = []
-  state.sale = false
-  const groupedActiveFilters = groupFiltersByKey(filters)
-
-  groupedActiveFilters.forEach(([key, value]: [keyof FilterState, any]) => {
-    state[key] =
-      typeof value[0].value === 'boolean' ? value[0].value === true : value
-  })
-}
-
-const setActivePriceRangeInState = (min: CentAmount, max: CentAmount) => {
-  if (min && minPrice.value && minPrice.value !== min) {
-    state.prices[0] = min
-  }
-
-  if (max && maxPrice.value && maxPrice.value !== max) {
-    state.prices[1] = max
-  }
-}
-
-const setStateFromUrlParams = () => {
-  setActiveFiltersInState(
-    getActiveFilters(availableFilterValues.value, activeFilters.value),
-  )
-
-  if (activeFilters.value.minPrice || activeFilters.value.maxPrice) {
-    setActivePriceRangeInState(
-      parseInt(activeFilters.value.minPrice) as CentAmount,
-      parseInt(activeFilters.value.maxPrice) as CentAmount,
-    )
-  } else {
-    state.prices = [minPrice.value, maxPrice.value]
-  }
-}
-
-const priceChanged = computed(
-  () => !isEqual(initialState.value.prices, state.prices),
-)
-
-const applyFilters = () => {
-  emitFilterEvent('filter:apply')
-  toggle()
-}
-
-const debouncedStateChangedEvent = useDebounce(
-  () => emitFilterEvent('filter:state-changed'),
-  50,
-)
-
-watch(state, () => {
-  debouncedStateChangedEvent()
+const { 
+  onSlideInOpen,
+  trackFilterFlyout,
+  resetFilters,
+  applyFilters,
+} = useFilterSlideIn({
+  filterableValues,
+  activeFilters,
+  onFilterApply: (filter) => emit('filter:apply', filter),
+  onFilterReset: () => emit('filter:reset'),
+  onStateChange: (filter) => emit('filter:state-changed', filter)
 })
 
-const emitFilterEvent = (event: 'filter:apply' | 'filter:state-changed') => {
-  emit(event, {
-    ...transformStateToFilters(useOmit(state, ['prices', 'sale'])),
-    ...(priceChanged.value && transformMinAndMaxPriceToFilter(state.prices)),
-    ...(state.sale && { sale: true }),
-  })
-}
-
-const resetFilter = (key: keyof FilterState) => {
-  // @ts-ignore
-  state[key] = [...initialState.value[key]]
-}
-
-const resetFilters = () => {
-  Object.assign(state, useClone(initialState.value))
-  emit('filter:reset')
-}
-
-const currentShop = useCurrentShop()
-
-const locale = currentShop.value!.locale?.replace('_', '-')
-const currencyCode = currentShop.value!.currency
-
-const isSaleActive = computed(
-  () =>
-    (availableFilterValues.value.sale || []).length &&
-    !!availableFilterValues.value.sale[0].count,
-)
-
-const onSlideInOpen = () => {
-  setStateFromUrlParams()
-  trackFilterFlyout('open', 'true')
-}
 </script>

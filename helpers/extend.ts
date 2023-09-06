f1b32110d50f75b9835fcec00c1906cb71783a05
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-06 19:44:13 +0200

    Moved helpers to utils directory & removed the helper plugin.

--- helpers/extend.ts
+++
@@ -1,6 +0,0 @@
-export default function extend(app: any, mixin: any) {
-  if (!app.mixins) {
-    app.mixins = []
-  }
-  app.mixins.push(mixin)
-}
--- helpers/ui.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  showDividerTag(index: number, arrayLength: number) {
-    return index >= 0 && arrayLength > 1 && index < arrayLength - 1
-  },
-}

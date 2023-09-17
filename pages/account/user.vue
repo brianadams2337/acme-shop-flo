b599e4eaf1c2864120bbb6175cd8df2f66bcdc36
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-17 23:15:41 +0200

    Add update password and personal info forms

--- pages/account/user.vue
+++ pages/account/user.vue
@@ -1,5 +1,8 @@
 <template>
-  <AccountWrapper :title="$t('my_account.profile_menu')" is-account-page>
+  <AccountWrapper
+    :title="$t('my_account.profile_menu')"
+    is-account-page
+    class="mb-6">
     <Headline
       class="mt-8 hidden md:block"
       :is-uppercase="false"
@@ -7,143 +10,13 @@
       tag="h3">
       {{ $t('my_account.profile_menu') }}
     </Headline>
-    <form class="mx-auto mt-10 flex w-full flex-col md:mx-0 lg:w-[400px]">
-      <RadioGroup
-        v-model="payload.gender"
-        :items="genders"
-        class="items-center" />
-      <div class="mt-8">
-        <ValidatedInputGroup v-slot="{ isValid }" :errors="v.firstName.$errors">
-          <TextInput
-            v-model="payload.firstName"
-            :has-errors="!isValid"
-            :placeholder="$t('form_fields.first_name')"
-            required
-            @input="v.firstName.$touch" />
-        </ValidatedInputGroup>
-        <ValidatedInputGroup v-slot="{ isValid }" :errors="v.lastName.$errors">
-          <TextInput
-            v-model="payload.lastName"
-            :has-errors="!isValid"
-            :placeholder="$t('form_fields.last_name')"
-            required
-            @input="v.lastName.$touch" />
-        </ValidatedInputGroup>
-
-        <ValidatedInputGroup v-slot="{ isValid }" :errors="v.birthDate.$errors">
-          <TextInput
-            v-model="payload.birthDate"
-            :placeholder="dobFormat.placeholder"
-            :mask="dobFormat.mask"
-            :has-errors="!isValid"
-            @input="v.birthDate.$touch" />
-        </ValidatedInputGroup>
-        <TextInput
-          v-model="payload.email"
-          :placeholder="$t('form_fields.email')"
-          :hint="$t('my_account.user.email_change_hint')"
-          readonly />
-
-        <!-- save -->
-        <div
-          class="mt-6 flex w-full items-center justify-center"
-          data-test-id="save-button">
-          <AppButton
-            class="w-full capitalize md:w-auto md:min-w-[50%]"
-            :disabled="isUpdating || v.$error"
-            @click="update">
-            {{ $t('my_account.user.save_label') }}
-          </AppButton>
-        </div>
-      </div>
-    </form>
-    <!-- <UpdatePasswordForm /> -->
+    <UserPersonalInfoForm />
+    <UpdatePasswordForm />
   </AccountWrapper>
 </template>
 
 <script setup lang="ts">
-import { formatDate, getPayloadDate } from '@scayle/storefront-nuxt'
-import useVuelidate from '@vuelidate/core'
-import { Action, dateOfBirthFormats } from '~/constants'
-
-const { $alert, $i18n, $validation } = useNuxtApp()
-const currentShop = useCurrentShop()
-
-const { user, updateUser } = await useUser()
-
-const dobFormat = computed(() => {
-  const locale = currentShop.value?.locale
-  return locale ? dateOfBirthFormats[locale] : dateOfBirthFormats.de_DE
-})
-
-// initializes and keeps the payload in sync with the user data
-const initPayload = () => ({
-  firstName: user.value?.firstName || '',
-  lastName: user.value?.lastName || '',
-  email: user.value?.email || '',
-  birthDate: formatDate(user.value?.birthDate) || '',
-  gender: user.value?.gender,
-})
-
-const payload = reactive(initPayload())
-
-const isUpdating = ref(false)
-
-const rules = computed(() => ({
-  firstName: {
-    name: $validation.rule.name,
-    required: $validation.rule.required,
-    maxLength: $validation.rule.maxLength(30),
-  },
-  lastName: {
-    name: $validation.rule.name,
-    required: $validation.rule.required,
-    maxLength: $validation.rule.maxLength(50),
-  },
-  birthDate: {
-    required: $validation.rule.required,
-    date: $validation.rule.date,
-    futureDate: $validation.rule.futureDate,
-  },
-}))
-
-const v = useVuelidate(rules.value, payload, { $scope: false })
-
-const setPayload = () => Object.assign(payload, initPayload())
-
-const genders = computed(() => [
-  {
-    label: $i18n.t('form_fields.male'),
-    value: 'm',
-  },
-  {
-    label: $i18n.t('form_fields.female'),
-    value: 'f',
-  },
-])
-
-watch(user, setPayload, { immediate: true })
-
-const update = async () => {
-  isUpdating.value = true
-
-  const saveError = $i18n.t('my_account.user.save_error')
-  try {
-    const isValid = await v.value.$validate()
-    if (!isValid) {
-      throw new Error(saveError)
-    }
-    await updateUser({
-      ...payload,
-      birthDate: getPayloadDate(payload.birthDate),
-    })
-    $alert.show($i18n.t('my_account.user.save_message'), Action.CONFIRM)
-  } catch {
-    $alert.show(saveError, Action.CONFIRM)
-  } finally {
-    isUpdating.value = false
-  }
-}
+await useUser()
 </script>
 
 <script lang="ts">

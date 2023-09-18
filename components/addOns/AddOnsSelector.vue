bc4a07d6c64d2bdc427922ba295aa65695ecc866
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-18 14:12:33 +0200

    Added AddOnSelector

---
+++ components/addOns/AddOnsSelector.vue
@@ -0,0 +1,73 @@
+<template>
+  <div v-if="computedAddOns.length" class="text-sm">
+    <div
+      v-for="addOn in computedAddOns"
+      :key="`${addOn.variantId}`"
+      class="mb-3 flex items-center last:mb-0">
+      <input
+        class="!focus:border-none m-0 mr-3 focus:outline-0"
+        type="checkbox"
+        @change="(event) => onSelectionChanged(event, addOn)" />
+      <p>
+        {{ addOn.label }}
+        {{
+          toCurrency(addOn.price, {
+            currency: 'EUR',
+            locale: 'de',
+          })
+        }}
+        <span class="text-2xs">
+          {{ $t('price.including_vat') }}
+        </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { getFirstAttributeValue } from '@scayle/storefront-nuxt'
+import { AddOnItem } from '~/types'
+
+const props = defineProps({
+  addOns: {
+    type: Array as PropType<number[]>,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['click:service-selection'])
+const { data: variants, refresh: fetchVariants } = await useVariant({
+  ids: props.addOns.map((addOn) => parseInt(addOn.toString())),
+})
+const productIds = variants.value?.map((variant) => variant.productId)
+const { data: products, refresh: fetchProducts } = await useProductsByIds({
+  ids: productIds,
+})
+const computedAddOns = ref<AddOnItem[]>([])
+
+onMounted(() => {
+  return resolveVariantInfo()
+})
+
+const onSelectionChanged = (event: any, { variantId }: AddOnItem) => {
+  emit('click:service-selection', {
+    isSelected: event.target.checked,
+    variantId,
+  })
+}
+
+const resolveVariantInfo = async () => {
+  await fetchVariants()
+  await fetchProducts()
+  // sorting by productId ensures 1-1 mapping by index for later computation of label and price
+  products.value?.sort((a, b) => a.id - b.id)
+  variants.value?.sort((a, b) => a.productId - b.productId)
+
+  computedAddOns.value = (products.value || []).map((product, idx) => ({
+    label:
+      getFirstAttributeValue(product.attributes, 'name')?.label ?? 'add on',
+    price: variants.value![idx]?.price.withoutTax,
+    variantId: variants.value![idx].id,
+  }))
+}
+</script>

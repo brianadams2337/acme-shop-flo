1913f511c3d5ea5c57491f07792ccc0e464d747e
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-10-02 10:55:08 +0200

    Move useTrackingEvents to the composables root dir

--- composables/tracking/index.ts
+++ composables/tracking/index.ts
@@ -1,4 +1,3 @@
 export * from './events'
 export * from './watchers'
 export { useCheckoutStepTrackingInterceptor } from './useCheckoutStepTrackingInterceptor'
-export { useTrackingEvents } from './useTrackingEvents'

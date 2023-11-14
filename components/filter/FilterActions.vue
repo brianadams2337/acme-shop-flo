9eb8199c47e9eef7f51f2d22a3a9e8d7f0920cdc
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-11-14 17:02:11 +0100

    Breakdown filter slide into smaller components

---
+++ components/filter/FilterActions.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid grid-cols-2 gap-2">
+    <AppButton
+      data-test-id="reset-filter-button"
+      type="tertiary"
+      class="text-sm !capitalize"
+      @click="resetFilters"
+      >{{ $t('filter.reset_all') }}
+    </AppButton>
+    <AppButton
+      data-test-id="apply-filter-button"
+      is-full-width
+      type="primary"
+      class="text-sm !capitalize"
+      @click="applyFilters()"
+    >
+      {{
+        filteredCount !== unfilteredCount
+          ? $t('filter.show_results_count', { count: filteredCount })
+          : $t('filter.show_results')
+      }}
+    </AppButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { resetFilters, applyFilters, unfilteredCount, filteredCount } =
+  await useFilter()
+</script>
--- components/filter/FilterSlideIn.vue
+++ components/filter/FilterSlideIn.vue
@@ -6,58 +6,17 @@
     @close="trackFilterFlyout('close', 'true')"
   >
     <template #slideInHeader="{ toggle: toggleItem }">
-      <div class="flex w-full items-center justify-between">
-        <Headline size="xl" tag="p">{{ $t('filter.title') }} </Headline>
-        <AppButton
-          type="ghost"
-          size="xs"
-          data-test-id="closeCross"
-          @click="toggleItem"
-        >
-          <template #icon="{ _class }">
-            <IconClose :class="_class" />
-          </template>
-        </AppButton>
-      </div>
+      <FiltersHeader :toggle-item="toggleItem" />
     </template>
     <template #slideInBody>
       <FilterSlideInBody />
     </template>
-
     <template #slideInActions>
-      <div class="grid grid-cols-2 gap-2">
-        <AppButton
-          data-test-id="reset-filter-button"
-          type="tertiary"
-          class="text-sm !capitalize"
-          @click="resetFilters"
-          >{{ $t('filter.reset_all') }}
-        </AppButton>
-        <AppButton
-          data-test-id="apply-filter-button"
-          is-full-width
-          type="primary"
-          class="text-sm !capitalize"
-          @click="applyFilters()"
-        >
-          {{
-            filteredCount !== unfilteredCount
-              ? $t('filter.show_results_count', { count: filteredCount })
-              : $t('filter.show_results')
-          }}
-        </AppButton>
-      </div>
+      <FilterActions />
     </template>
   </SlideIn>
 </template>
 
 <script setup lang="ts">
-const {
-  onSlideInOpen,
-  trackFilterFlyout,
-  resetFilters,
-  applyFilters,
-  unfilteredCount,
-  filteredCount,
-} = await useFilter()
+const { onSlideInOpen, trackFilterFlyout } = await useFilter()
 </script>

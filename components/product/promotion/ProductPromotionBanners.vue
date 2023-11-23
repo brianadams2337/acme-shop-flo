c3e79971a0fdb0d38da8a16b81345cac23d63c61
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-11-23 09:02:46 +0100

    Add highest priority label

--- components/product/promotion/ProductPromotionBanners.vue
+++ components/product/promotion/ProductPromotionBanners.vue
@@ -4,9 +4,10 @@
     class="flex w-full max-w-sm flex-col"
   >
     <PromotionItemContent
-      v-for="{ id, customData, schedule } in applicablePromotions"
+      v-for="{ id, customData, schedule, priority } in applicablePromotions"
       :key="id"
       v-bind="{ customData, schedule }"
+      :is-priority-badge-shown="isHighestPriority(priority)"
       class="mb-2 w-full"
     />
   </div>
@@ -17,7 +18,12 @@ import type { Product } from '@scayle/storefront-nuxt'
 
 const props = defineProps<{ product: Product }>()
 
-const { applicablePromotions } = await useProductPromotions(props.product)
+const { applicablePromotions, highestPriorityPromotion } =
+  await useProductPromotions(props.product)
 
 const { promotionEngineFeatureEnabled } = useRuntimeConfig().public
+
+const isHighestPriority = (priority: number) => {
+  return highestPriorityPromotion.value.priority === priority
+}
 </script>

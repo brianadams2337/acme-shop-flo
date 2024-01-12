6bc64da7fff64cd47baf317401976b18c0d2cf89
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-01-12 10:20:13 +0100

    feat: fix cms fixes

--- modules/cms/providers/storyblok/composables/useCMS.ts
+++ modules/cms/providers/storyblok/composables/useCMS.ts
@@ -1,66 +1,33 @@
-import type { StoryblokStory } from 'storyblok-generate-ts'
-import type { NuxtError } from 'nuxt/app'
-import type { ISbStoriesParams, ISbError } from 'storyblok-js-client'
+import type { ISbStoriesParams } from 'storyblok-js-client'
 
-type Status = 'idle' | 'pending' | 'success' | 'error'
-
-const handleCmsError = (error: any): NuxtError => {
-  const { status, message } = JSON.parse(error) as ISbError
-  return createError({ statusCode: status, statusMessage: message })
-}
-
-export default function useCMS<T = unknown>(key: string) {
-  const log = useLog(`useCMS ${key}`)
-  const data = useState<StoryblokStory<T>>(`cms-data-${key}`)
-  const fetching = useState<boolean>(`fetching-${key}`)
-  const status = useState<Status>(`status-${key}`, () => 'idle')
-  const error = useState<NuxtError | undefined>(`error-${key}`, () => undefined)
-
+export default function useCMS<_T = unknown>(key: string) {
   const storyblokApi = useStoryblokApi()
   const storyblokOptions = useDefaultStoryblokOptions()
 
   async function fetchBySlug(slug: string) {
-    status.value = 'pending'
-    fetching.value = true
-
-    try {
-      const { data: storyData } = await storyblokApi.get(
-        `cdn/stories/${slug}`,
-        storyblokOptions,
-      )
-
-      data.value = storyData.story
-    } catch (e) {
-      error.value = handleCmsError(e)
-      log.error(`Error fetching CMS Slug: ${slug}`, e)
-    } finally {
-      fetching.value = false
-      status.value = error.value ? 'error' : 'success'
-    }
+    return await useAsyncData(
+      key,
+      () => storyblokApi.get(`cdn/stories/${slug}`, storyblokOptions),
+      {
+        immediate: false,
+      },
+    )
   }
 
   async function fetchByFolder(folder: string, options?: ISbStoriesParams) {
-    fetching.value = true
-    status.value = 'pending'
-
-    try {
-      const {
-        data: { stories },
-      } = await storyblokApi.getStories({
-        ...storyblokOptions,
-        starts_with: folder,
-        ...options,
-      })
-
-      data.value = stories as unknown as StoryblokStory<T>
-    } catch (e) {
-      error.value = handleCmsError(e)
-      log.error(`Error fetching CMS Folder`, e)
-    } finally {
-      fetching.value = false
-      status.value = error.value ? 'error' : 'success'
-    }
+    return await useAsyncData(
+      key,
+      () =>
+        storyblokApi.getStories({
+          ...storyblokOptions,
+          starts_with: folder,
+          ...options,
+        }),
+      {
+        immediate: false,
+      },
+    )
   }
 
-  return { fetchBySlug, data, fetchByFolder, fetching, status, error }
+  return { fetchBySlug, fetchByFolder }
 }

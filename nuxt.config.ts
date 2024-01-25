285ae1e307e1941fef70e7d2673aeef5ff29ff19
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-01-25 05:54:22 +0100

    refactor(nuxt): set nuxt debug to false by default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,7 +55,7 @@ export default defineNuxtConfig({
   telemetry: false,
 
   // https://nuxt.com/docs/api/nuxt-config#debug
-  debug: true,
+  debug: false,
 
   // Any key/value pair outside of the `public` key are private/server-side only
   // https://nuxt.com/docs/guide/going-further/runtime-config

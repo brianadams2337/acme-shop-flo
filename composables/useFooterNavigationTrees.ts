1d7cdf056f1cb3e49fd79ee26ba3655fd43a3ae3
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-02-21 09:08:07 +0100

    Add category with param

--- composables/useFooterNavigationTrees.ts
+++ composables/useFooterNavigationTrees.ts
@@ -1,7 +1,10 @@
 import type { NavigationTree } from '@scayle/storefront-nuxt'
 
 export async function useFooterNavigationTrees() {
-  const { data } = await useNavigationTrees()
+  const { data } = await useNavigationTrees({
+    params: { params: { with: { category: true } } },
+    key: 'footer-navigaton-trees',
+  })
 
   const footerNavigationTrees = computed<NavigationTree[]>(() => {
     return filterNavigationTree(data.value, 'footer')
--- composables/useHeaderNavigationTrees.ts
+++ composables/useHeaderNavigationTrees.ts
@@ -1,7 +1,10 @@
 import type { NavigationTree } from '@scayle/storefront-nuxt'
 
 export async function useHeaderNavigationTrees() {
-  const { data } = await useNavigationTrees()
+  const { data } = await useNavigationTrees({
+    params: { params: { with: { category: true } } },
+    key: 'header-navigaton-trees',
+  })
 
   const headerNavigationTrees = computed<NavigationTree[]>(() => {
     return filterNavigationTree(data.value, 'header')

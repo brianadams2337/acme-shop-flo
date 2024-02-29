25b7ede61883cf40047efe176b3b4025a34e7927
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-02-29 16:37:08 +0100

    fix(nuxt): test global setup plugin just load user

--- plugins/globalSetup.ts
+++ plugins/globalSetup.ts
@@ -4,7 +4,4 @@ export default defineNuxtPlugin(async () => {
   // Initialize data
   console.log('Global Setup')
   await useUser()
-  await useWishlist()
-  await useBasket()
-  await useRootCategories()
 })

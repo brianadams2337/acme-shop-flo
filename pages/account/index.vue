09c5edff8f9d0b8125fcb6cc3004200092cff80f
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-04-16 11:49:30 +0200

    refactor(sfb): tweak account index page to be visually consistent with other account page layouts

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,35 +1,37 @@
 <template>
-  <div>
+  <div class="container my-0 overflow-hidden px-0 max-sm:max-w-none">
     <AccountHeader
       :title="$t('navigation.my_account')"
       icon="IconUserSecondary"
     />
-    <div
-      v-if="user"
-      class="flex flex-col justify-center space-x-0 space-y-4 px-4 py-8 sm:flex-row sm:space-x-8 sm:space-y-0"
-    >
-      <ContainerLink
-        :to="routeList.orders"
-        :header="$t('my_account.orders_menu')"
-        :subheader="$t('my_account.orders_count', { count: orderCount })"
-        class="w-full"
-      />
-      <ContainerLink
-        :to="routeList.user"
-        :header="$t('my_account.profile_menu')"
-        :subheader="$t('my_account.personal_data_title')"
-        class="w-full"
-      />
-      <ContainerLink
-        :to="routeList.subscriptionOverview"
-        :header="$t('my_account.subscriptions_menu')"
-        :subheader="$t('my_account.subscriptions_subtitle')"
-        class="w-full"
-      />
-    </div>
-    <div class="mt-2 px-4">
-      <LogoutButton data-test-id="add-item-to-basket-button" />
-    </div>
+    <PageContent>
+      <div
+        v-if="user"
+        class="flex flex-col justify-center space-x-0 space-y-4 sm:flex-row sm:space-x-8 sm:space-y-0"
+      >
+        <ContainerLink
+          :to="routeList.orders"
+          :header="$t('my_account.orders_menu')"
+          :subheader="$t('my_account.orders_count', { count: orderCount })"
+          class="w-full"
+        />
+        <ContainerLink
+          :to="routeList.user"
+          :header="$t('my_account.profile_menu')"
+          :subheader="$t('my_account.personal_data_title')"
+          class="w-full"
+        />
+        <ContainerLink
+          :to="routeList.subscriptionOverview"
+          :header="$t('my_account.subscriptions_menu')"
+          :subheader="$t('my_account.subscriptions_subtitle')"
+          class="w-full"
+        />
+      </div>
+      <div class="mt-4">
+        <LogoutButton data-test-id="add-item-to-basket-button" />
+      </div>
+    </PageContent>
   </div>
 </template>
 

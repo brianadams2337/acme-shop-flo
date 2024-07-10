9b8d9d4e617f1fee91aa0e8224dc1831abdcdd80
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-07-10 12:52:43 +0200

    Show IDP Login buttons in RegisterForm.vue

---
+++ .changeset/popular-poets-fail.md
@@ -0,0 +1,5 @@
+---
+'@scayle/storefront-boilerplate-nuxt': minor
+---
+
+Show IDP login buttons onin RegisterForm.vue
--- components/auth/RegisterForm.vue
+++ components/auth/RegisterForm.vue
@@ -1,75 +1,83 @@
 <template>
-  <form>
-    <div class="h-20">
-      <SalutationSelect v-model="editableUser.gender" />
-    </div>
+  <div class="flex flex-col gap-6">
+    <form>
+      <div class="h-20">
+        <SalutationSelect v-model="editableUser.gender" />
+      </div>
 
-    <SFValidatedInputGroup v-slot="{ isValid }" :errors="v.first_name.$errors">
-      <SFTextInput
-        v-model="editableUser.first_name"
-        autocomplete="given-name"
-        :placeholder="$t('form_fields.first_name')"
-        :has-errors="!isValid"
-        required
-        :readonly="isSubmitting"
-        @change="v.first_name.$touch()"
-      />
-    </SFValidatedInputGroup>
+      <SFValidatedInputGroup
+        v-slot="{ isValid }"
+        :errors="v.first_name.$errors"
+      >
+        <SFTextInput
+          v-model="editableUser.first_name"
+          autocomplete="given-name"
+          :placeholder="$t('form_fields.first_name')"
+          :has-errors="!isValid"
+          required
+          :readonly="isSubmitting"
+          @change="v.first_name.$touch()"
+        />
+      </SFValidatedInputGroup>
 
-    <SFValidatedInputGroup v-slot="{ isValid }" :errors="v.last_name.$errors">
-      <SFTextInput
-        v-model="editableUser.last_name"
-        autocomplete="family-name"
-        :placeholder="$t('form_fields.last_name')"
-        :has-errors="!isValid"
-        required
-        :readonly="isSubmitting"
-        @change="v.last_name.$touch()"
-      />
-    </SFValidatedInputGroup>
+      <SFValidatedInputGroup v-slot="{ isValid }" :errors="v.last_name.$errors">
+        <SFTextInput
+          v-model="editableUser.last_name"
+          autocomplete="family-name"
+          :placeholder="$t('form_fields.last_name')"
+          :has-errors="!isValid"
+          required
+          :readonly="isSubmitting"
+          @change="v.last_name.$touch()"
+        />
+      </SFValidatedInputGroup>
 
-    <SFValidatedInputGroup v-slot="{ isValid }" :errors="v.email.$errors">
-      <SFTextInput
-        v-model="editableUser.email"
-        autocomplete="usernaasdme"
-        :placeholder="$t('form_fields.email')"
-        type="email"
-        :has-errors="!isValid"
-        required
-        :readonly="isSubmitting"
-        @change="v.email.$touch()"
-      />
-    </SFValidatedInputGroup>
+      <SFValidatedInputGroup v-slot="{ isValid }" :errors="v.email.$errors">
+        <SFTextInput
+          v-model="editableUser.email"
+          autocomplete="usernaasdme"
+          :placeholder="$t('form_fields.email')"
+          type="email"
+          :has-errors="!isValid"
+          required
+          :readonly="isSubmitting"
+          @change="v.email.$touch()"
+        />
+      </SFValidatedInputGroup>
 
-    <SFValidatedInputGroup v-slot="{ isValid }" :errors="v.password.$errors">
-      <SFTextInput
-        v-model="editableUser.password"
-        autocomplete="new-password"
-        :placeholder="$t('form_fields.password')"
-        type="password"
-        :has-errors="!isValid"
-        required
-        :readonly="isSubmitting"
-        @change="v.password.$touch()"
-      />
-    </SFValidatedInputGroup>
+      <SFValidatedInputGroup v-slot="{ isValid }" :errors="v.password.$errors">
+        <SFTextInput
+          v-model="editableUser.password"
+          autocomplete="new-password"
+          :placeholder="$t('form_fields.password')"
+          type="password"
+          :has-errors="!isValid"
+          required
+          :readonly="isSubmitting"
+          @change="v.password.$touch()"
+        />
+      </SFValidatedInputGroup>
 
-    <SFButton
-      class="mt-3 w-full"
-      :disabled="isSubmitting"
-      :loading="isSubmitting"
-      @click="onSubmit"
-    >
-      {{ $t('login_page.sign_up.submit') }}
-    </SFButton>
-  </form>
+      <SFButton
+        class="mt-3 w-full"
+        :disabled="isSubmitting"
+        :loading="isSubmitting"
+        @click="onSubmit"
+      >
+        {{ $t('login_page.sign_up.submit') }}
+      </SFButton>
+    </form>
+    <IDPForm v-if="hasIDPs" :redirects="externalIDPRedirects" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
 import useVuelidate from '@vuelidate/core'
 import type { Gender } from '@scayle/storefront-nuxt'
+import { useRoute } from '#app/composables/router'
 import { useAuthentication, useValidationRules } from '~/composables'
+import { useIDP } from '#storefront/composables'
 
 const { register, isSubmitting } = useAuthentication('sign_up')
 const validationRules = useValidationRules()
@@ -109,5 +117,24 @@ const v = useVuelidate(
     },
   },
   editableUser,
+)
+
+const route = useRoute()
+
+const idpParams = computed(() => {
+  if (typeof route.query.redirectUrl === 'string') {
+    return {
+      queryParams: { redirectUrl: route.query.redirectUrl },
+    }
+  }
+
+  return { queryParams: undefined }
+})
+const { data: externalIDPRedirects } = useIDP(idpParams)
+
+const hasIDPs = computed(
+  () =>
+    externalIDPRedirects.value &&
+    Object.keys(externalIDPRedirects.value).length > 0,
 )
 </script>

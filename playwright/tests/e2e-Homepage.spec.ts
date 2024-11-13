4cf81fc8c341f435f086ce7475bfba0f52f14a61
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-11-13 14:30:48 +0000

    Test/homepage links fix

--- playwright/tests/e2e-Homepage.spec.ts
+++ playwright/tests/e2e-Homepage.spec.ts
@@ -19,16 +19,30 @@ test('C2141227 Verify Homepage sections', async ({
   }).toPass()
 })
 
-test('C2141228 Verify Homepage links', async ({ page }) => {
+test('C2141228 Verify Homepage links', async ({ page, context }) => {
+  test.setTimeout(60000)
   const linkUrls = await getAllLinksFromPage(page)
 
   for (const url of linkUrls) {
     await test.step(`Checking link: ${url}`, async () => {
       try {
-        const response = await page.request.get(url)
-        expect.soft(response.ok(), `${url} has error status code`).toBeTruthy()
-      } catch {
-        expect.soft(null, `request for ${url} failed`).toBeTruthy()
+        const response = await page.request.head(url)
+        expect(response.status()).toBeLessThan(400)
+        expect(response.statusText()).toBe('OK')
+      } catch (error) {
+        console.warn(
+          `HEAD request failed for ${url}, attempting full page navigation.`,
+        )
+        console.error('Error details:', error)
+        try {
+          const newPage = await context.newPage()
+          await newPage.goto(url)
+          await newPage.waitForLoadState('networkidle')
+          await newPage.close()
+        } catch (navError) {
+          console.error(`[Error] Navigation failed for ${url}:`, navError)
+          throw new Error(`Both HEAD request and navigation failed for ${url}`)
+        }
       }
     })
   }

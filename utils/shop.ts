aef5548ebf8729b69c0af760d92db5563405d05d
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2025-01-22 13:51:32 +0100

    Add doc comments to shop utils

--- utils/shop.ts
+++ utils/shop.ts
@@ -1,6 +1,18 @@
 import type { PublicShopConfig } from '@scayle/storefront-nuxt'
 
-export function getShopName(locale: string, includeLanguage: boolean = false) {
+/**
+ * Get a string that describes a shop based on its locale.
+ * It will include the shop's region, translated to the shop's language
+ * and optionally the shop's language as well.
+ *
+ * @param locale the locale of the shop
+ * @param includeLanguage whether to include the language in the name
+ * @returns the name of the shop
+ */
+export function getShopName(
+  locale: string,
+  includeLanguage: boolean = false,
+): string | undefined {
   const [languageCode, regionCode] = locale.split('-')
 
   const region = new Intl.DisplayNames([locale], {
@@ -10,6 +22,12 @@ export function getShopName(locale: string, includeLanguage: boolean = false) {
   return includeLanguage ? `${region} | ${languageCode.toUpperCase()}` : region
 }
 
+/**
+ * Determine if there are multiple shops with the same country/region
+ *
+ * @param shops the list of shops to check
+ * @returns true if there is a shop which shares its country/region with another shop. Otherwise, false.
+ */
 export function hasMultipleShopsForCountry(
   shops: Pick<PublicShopConfig, 'locale'>[],
 ): boolean {

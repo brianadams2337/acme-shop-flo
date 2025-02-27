07658d2862695bdcedb98c966621fc458c67d7cc
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2025-02-27 12:27:47 +0000

    chore(sfb): remove auto-import option from UI module

---
+++ .changeset/major-camels-pay.md
@@ -0,0 +1,5 @@
+---
+'@scayle/storefront-boilerplate-nuxt': minor
+---
+
+**\[Storefront-UI\]** Fully remove auto-import capabilities of local UI module.
--- modules/ui/index.ts
+++ modules/ui/index.ts
@@ -1,15 +1,8 @@
-import {
-  addImportsDir,
-  createResolver,
-  defineNuxtModule,
-  addComponentsDir,
-} from '@nuxt/kit'
-
+import { createResolver, defineNuxtModule } from '@nuxt/kit'
 import type { Breakpoints } from '@vueuse/core'
 
 export type ModuleOptions = {
   prefix?: string
-  autoImports?: boolean
   breakpoints?: Breakpoints
 }
 
@@ -25,21 +18,6 @@ export default defineNuxtModule<ModuleOptions>({
   },
   async setup(options, nuxt) {
     const { resolve } = createResolver(import.meta.url)
-
-    if (options.autoImports) {
-      await addComponentsDir({
-        path: resolve('./runtime/components'),
-        pathPrefix: false,
-        prefix: options.prefix ?? 'SF',
-        // Only auto import Vue components
-        // Resolves:
-        // > This module cannot be imported in the Vue part of your app.
-        // > [importing @nuxt/test-utils/runtime from modules/ui/runtime/components/core/SFButton.nuxt.test.ts]
-        extensions: ['vue'],
-      })
-      addImportsDir(resolve('./runtime/composables'))
-      addImportsDir(resolve('./runtime/helpers'))
-    }
 
     nuxt.options.runtimeConfig.public.storefrontUI = {
       breakpoints: options.breakpoints ?? {

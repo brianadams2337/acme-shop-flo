ffcf77ee53fd5705d53265a724fd93e53a9648cd
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2025-04-28 08:40:01 +0200

    feat(nuxt): Provide storage via plugin

---
+++ .changeset/public-lands-double.md
@@ -0,0 +1,5 @@
+---
+'@scayle/storefront-application-nuxt': minor
+---
+
+**\[Config\]** Implement the server plugin (`./server/plugins/storageConfig.ts`) to offer preconfigured storage for `@scayle/storefront-nuxt`, enabling the use of any unstorage driver.
---
+++ server/plugins/storageConfig.ts
@@ -0,0 +1,50 @@
+import scayleKvDriver from '@scayle/unstorage-scayle-kv-driver'
+import compressionDriver from '@scayle/unstorage-compression-driver'
+import vercelKV from 'unstorage/drivers/vercel-kv'
+// NOTE: We need to import here from the Nuxt server-specific #imports to mitigate
+// unresolved dependencies in the imported composables from Nitro(nitropack).
+// This results in `nuxi typecheck` not being able to properly infer the correct
+// import and throw an error without explicit `@ts-expect-error`
+// @ts-expect-error: TS2578 Cand find defineNitroPlugin and useStorage as exported members of '#imports'
+import { defineNitroPlugin, useStorage } from '#imports'
+/**
+ * This plugin configures the storage for the Storefront.
+ * It mounts the storage drivers for the Storefront session and cache.
+ *
+ * @see https://scayle.dev/en/storefront-guide/developer-guide/technical-foundation/storage
+ */
+export default defineNitroPlugin(() => {
+  const storage = useStorage()
+
+  // This is only needed when one of the target environments is Vercel.
+  // If none of the target environments is Vercel, this can be removed.
+  if (import.meta.preset?.includes('vercel')) {
+    storage.mount('storefront-session', vercelKV({}))
+    storage.mount(
+      'storefront-cache',
+      compressionDriver({
+        encoding: 'gzip',
+        passthroughDriver: vercelKV({}),
+      }),
+    )
+    return
+  }
+
+  storage.mount(
+    'storefront-session',
+    scayleKvDriver({
+      disableClusterMode: import.meta.dev,
+    }),
+  )
+
+  storage.mount(
+    'storefront-cache',
+    compressionDriver({
+      encoding: 'brotli',
+      passthroughDriver: scayleKvDriver({
+        disableClusterMode: import.meta.dev,
+        ttl: 10 * 60,
+      }),
+    }),
+  )
+})

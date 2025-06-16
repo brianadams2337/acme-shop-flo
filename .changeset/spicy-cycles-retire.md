9a1903b149fb8e7718e557b51c8690cf6a9d605f
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2025-06-16 11:46:12 +0200

    Show active sorting as first item in SFMobileSortSelection.vue

---
+++ .changeset/spicy-cycles-retire.md
@@ -0,0 +1,5 @@
+---
+'@scayle/storefront-application-nuxt': patch
+---
+
+**\[UI\]** Updated `SFMobileSortSelection.vue` so that all sorting options are visible at once. This ensures users can easily identify the active sorting without scrolling.
--- components/sorting/SFMobileSortSelection.vue
+++ components/sorting/SFMobileSortSelection.vue
@@ -1,11 +1,11 @@
 <template>
-  <SFItemsSlider mode="horizontal" data-testid="sorting-items-slider">
+  <div class="flex flex-wrap">
     <SFLocalizedLink
       v-for="sort in sortLinks"
       :key="sort.key"
       :to="sort.to"
       raw
-      class="mr-2 flex h-10 w-fit items-center justify-center whitespace-pre rounded-full border-2 border-transparent bg-gray-100 px-4 text-md font-semibold text-secondary transition-none"
+      class="mb-2 mr-2 flex h-10 w-fit items-center justify-center whitespace-pre rounded-full border-2 border-transparent bg-gray-100 px-4 text-md font-semibold text-secondary transition-none"
       :class="{
         '!border-accent bg-white !text-accent': sort.key === selectedSort?.key,
       }"
@@ -13,13 +13,12 @@
     >
       {{ $t(sort.label) }}
     </SFLocalizedLink>
-  </SFItemsSlider>
+  </div>
 </template>
 
 <script setup lang="ts">
 import SFLocalizedLink from '../SFLocalizedLink.vue'
 import { useTrackingEvents } from '~/composables'
-import { SFItemsSlider } from '#storefront-ui/components'
 import { useProductListSort } from '#storefront-product-listing'
 import { useRoute } from '#app/composables/router'
 
